847cfef3c355db9bcce4a1e0807fbb53b3328abb
Lex Trofimov <daslef93@gmail.com>
2025-03-17 14:39:56 +0300

    connection, schema & example of queries

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,31 +1,28 @@
-import type { Insertable } from "kysely"
-import type getConnection from "./connection"
-import type { DB } from "kysely-codegen"
+import type { Insertable } from "kysely";
+import type getConnection from "./connection";
+import type { DB } from "kysely-codegen";
 
-type Connection = ReturnType<typeof getConnection>
+type Connection = ReturnType<typeof getConnection>;
 
 async function logAllQuestions(connection: Connection) {
-  try {
-    const result = await connection.selectFrom("Question").selectAll().execute()
-    console.log(result)
-  } catch (error) {
-    console.error(error)
-  }
+	try {
+		const result = await connection.selectFrom("Question").selectAll().execute();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-async function createSurvey(
-  connection: Connection,
-  newSurvey: Insertable<DB["Survey"]>
-) {
-  try {
-    await connection
-      .insertInto("Survey")
-      .values(newSurvey)
-      .returningAll()
-      .executeTakeFirstOrThrow()
-  } catch (error) {
-    console.error(error)
-  }
+async function createSurvey(connection: Connection, newSurvey: Insertable<DB["Survey"]>) {
+	try {
+		await connection
+			.insertInto("Survey")
+			.values(newSurvey)
+			.returningAll()
+			.executeTakeFirstOrThrow();
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-export { logAllQuestions, createSurvey }
+export { logAllQuestions, createSurvey };
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import getConnection from "./database/connection";
 import { logAllQuestions, createSurvey } from "./database/queries";
 
 export async function main() {
-  try {
-    const connection = getConnection()
-    await logAllQuestions(connection)
-    await createSurvey(connection, { Description: "Insertable Test" })
-  } catch (error) {
-    console.error("Failed to start:", error);
-    process.exit(1);
-  }
+	try {
+		const connection = getConnection();
+		await logAllQuestions(connection);
+		await createSurvey(connection, { Description: "Insertable Test" });
+	} catch (error) {
+		console.error("Failed to start:", error);
+		process.exit(1);
+	}
 }
 
 main();
