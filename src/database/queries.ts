c97be70f29fd173d0158c25444e84f36c5869657
Lex Trofimov <daslef93@gmail.com>
2025-03-24 15:03:17 +0300

    remove redundant code

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,9 +1,9 @@
-import type { UpdateAnswer, MostFrequentQuestions } from "./types"
+import type { Question } from "./types"
 import type getConnection from "./connection"
 
 async function logAllQuestions(connection: ReturnType<typeof getConnection>) {
   try {
-    const result = await connection.selectFrom("Question").selectAll().execute()
+    const result = await connection.selectFrom("Question").selectAll().execute() as Question[]
     console.log(result)
   } catch (error) {
     console.error(error)
--- src/database/types.ts
+++ src/database/types.ts
@@ -1,5 +1,4 @@
-import { Selectable, Updateable } from 'kysely'
-import { Database } from "sqlite3";
+import type { Selectable, Updateable } from 'kysely'
 
 type SurveyID = 2014 | 2016 | 2017 | 2018 | 2019
 
@@ -26,20 +25,6 @@ interface AnswerSchema {
   UserID: number;
 }
 
-// interface UpdateAnswer {
-//   connection: Database;
-//   userId: number;
-//   questionId: number;
-//   surveyId: SurveyID;
-//   updatedAnswer: string;
-// }
-
-interface MostFrequentQuestions {
-  connection: Database;
-  quantity?: number;
-  cb: (rows: unknown[]) => void
-}
-
 export type Question = Selectable<QuestionSchema>
 export type UpdateAnswer = Updateable<AnswerSchema>
-export type { MostFrequentQuestions, DatabaseSchema }
+export type { DatabaseSchema }
