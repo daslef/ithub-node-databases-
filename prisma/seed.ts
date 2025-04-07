0bb69a35765d5a5703f62f45fe770bbb9e228939
Lex Trofimov <daslef93@gmail.com>
2025-04-07 19:17:28 +0300

    neon database

---
+++ prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import authors from './authors.json'
+import files from './files.json'
+
+async function seedAuthors(prismaClient: PrismaClient) {
+  await prismaClient.author.createMany({
+    data: authors.map(author => ({
+      id: author.id,
+      name: author.name,
+      created_at: new Date(author.created_at).toISOString(),
+      modified_at: new Date(author.modified_at).toISOString(),
+    }))
+  })
+}
+
+async function seedFiles(prismaClient: PrismaClient) {
+  await prismaClient.file.createMany({
+    data: files.map(({ created_at, modified_at, ...other }) => ({
+      ...other,
+      created_at: new Date(created_at).toISOString(),
+      modified_at: new Date(modified_at).toISOString()
+    }))
+  })
+}
+
+async function main() {
+  const prisma = new PrismaClient()
+  try {
+    await seedAuthors(prisma)
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    await seedFiles(prisma)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
